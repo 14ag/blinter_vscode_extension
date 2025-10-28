698d5ba44931fe433b638a2fd61de238e0545d09
14ag <muriukipn@gmail.com>
2025-10-28 23:15:23 +0300

    28-10

--- test/extension.test.js
+++ test/extension.test.js
@@ -11,18 +11,20 @@ try {
   console.log('Skipping extension integration tests: vscode module not available in this environment.');
   // Export an empty module so Mocha exits cleanly.
   module.exports = {};
-  return;
+  // Do not use `return` at top-level; guard runtime behavior by checking `vscode` below.
 }
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 // const myExtension = require('../extension');
 
-suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+if (vscode) {
+  suite('Extension Test Suite', () => {
+    vscode.window.showInformationMessage('Start all tests.');
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-	});
-});
+    test('Sample test', () => {
+      assert.strictEqual(-1, [1, 2, 3].indexOf(5));
+      assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+    });
+  });
+}
